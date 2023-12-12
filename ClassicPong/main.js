fd10b645469c0b66f65df24db605bde61b564ac3
lmaurin- <leonard.maurin@gmail.com>
2023-12-12 15:57:43 +0100

    organized a bit the code, classic pong finished

--- ClassicPong/main.js
+++ ClassicPong/main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { TextGeometry } from '../node_modules/three/examples/jsm/geometries/TextGeometry.js';
 import { FontLoader } from '../node_modules/three/examples/jsm/loaders/FontLoader.js';
+import { createTextMesh } from './Text.js';
 import {Player} from './Player.js'
 import * as material from './Materials.js'
 import {Wall} from './Arena.js'
@@ -30,8 +30,8 @@ function handleKeyUp(event) {
 	keys[event.code] = false;
 }
 
-
-function init(){
+function init()
+{
 	//Renderer
 	renderer = new THREE.WebGLRenderer({alpha: false, antialias: false});
 	renderer.setPixelRatio(devicePixelRatio / 2);
@@ -52,56 +52,25 @@ function init(){
 		);
 		camera.position.z = constants.CAMERA_STARTPOS_Z
 		
-	player_one = new Player((constants.GAME_AREA_WIDTH * -1) + 10, 0, constants.PADDLE_WIDTH, constants.PADDLE_HEIGHT, 0xffffff)
-	player_two = new Player(constants.GAME_AREA_WIDTH - 10, 0, constants.PADDLE_WIDTH, constants.PADDLE_HEIGHT, 0xffffff)
-	scene.add(player_one.mesh)
-	scene.add(player_two.mesh)
-	ball = new Ball()
-	scene.add(ball.mesh)
-	scene.add(ball.light)
-	
-}
-
-function createTextMesh(text, textMesh, x, y, z, fontcolor, fontsize) {
-	// Remove old text mesh if it exists
-	if (textMesh) {
-		textMesh.geometry.dispose()
-		textMesh.material.dispose()
-		scene.remove(textMesh);
-	}
-	// Create TextGeometry
-	var textGeometry = new TextGeometry(text, {
-		font: droidFont,
-		size: fontsize,
-		height: 1,
-		curveSegments: 1,
-		bevelEnabled: true,
-		bevelThickness: 1,
-		bevelSize: 1,
-		bevelOffset: 0,
-		bevelSegments: 1
-	});
-	
-	// Material
-	var textMaterial = new THREE.MeshBasicMaterial({ color: fontcolor });
-	textGeometry.center();
-
-	// Create mesh
-	var textMesh = new THREE.Mesh(textGeometry, textMaterial);
-
-	// Position and add to scene
-	textMesh.position.set(x, y, z);
-	scene.add(textMesh);
-
-	return textMesh
 }
 
 function initArena()
 {
+	//Adding players
+	player_one = new Player((constants.GAME_AREA_WIDTH * -1) + 10, 0, constants.PADDLE_WIDTH, constants.PADDLE_HEIGHT, 0xffffff)
+	player_two = new Player(constants.GAME_AREA_WIDTH - 10, 0, constants.PADDLE_WIDTH, constants.PADDLE_HEIGHT, 0xffffff)
+	scene.add(player_one.mesh, player_two.mesh)
+
+	//Adding the ball
+	ball = new Ball()
+	scene.add(ball.mesh, ball.light)
+
+	//Adding the floor and roof
 	var upper_wall = new Wall(constants.GAME_AREA_HEIGHT, 300, material.wallMaterial)
 	var lower_wall = new Wall(constants.GAME_AREA_HEIGHT * -1, 300, material.wallMaterial)
 	scene.add(upper_wall.mesh, lower_wall.mesh)
 	
+	//Creating and adding the two player goals
 	var player_one_goal = new THREE.Mesh(
 		new THREE.PlaneGeometry(20, constants.GAME_AREA_HEIGHT * 2, 1, 4),
 		material.wallMaterial)
@@ -113,9 +82,11 @@ function initArena()
 	player_two_goal.rotation.y = (Math.PI / 2) * -1
 	player_two_goal.position.x = constants.GAME_AREA_WIDTH
 	scene.add(player_one_goal, player_two_goal)
-	player_one_score_text = createTextMesh(player_one.score.toString(), player_one_score_text, (constants.GAME_AREA_WIDTH / 2) * -1, 0,-80, 0xf0f0f0, 50);
-	player_two_score_text = createTextMesh(player_two.score.toString(), player_two_score_text, constants.GAME_AREA_WIDTH / 2, 0,-80, 0xf0f0f0, 50);
 
+	//Initiating the score text meshes
+	player_one_score_text = createTextMesh(droidFont, player_one.score.toString(), player_one_score_text, (constants.GAME_AREA_WIDTH / 2) * -1, 0,-80, 0xf0f0f0, 50);
+	player_two_score_text = createTextMesh(droidFont, player_two.score.toString(), player_two_score_text, constants.GAME_AREA_WIDTH / 2, 0,-80, 0xf0f0f0, 50);
+	scene.add(player_one_score_text, player_two_score_text)
 }
 
 function initControls(){
@@ -135,13 +106,15 @@ function handle_scores()
 	{
 		player_one.score_point()
 		scene.remove(player_one_score_text)
-		player_one_score_text = createTextMesh(player_one.score.toString(), player_one_score_text, (constants.GAME_AREA_WIDTH / 2) * -1, 0,-80, 0xf0f0f0, 50);
+		player_one_score_text = createTextMesh(droidFont, player_one.score.toString(), player_one_score_text, (constants.GAME_AREA_WIDTH / 2) * -1, 0,-80, 0xf0f0f0, 50);
+		scene.add(player_one_score_text)
 	}
 	else
 	{
 		player_two.score_point()
 		scene.remove(player_two_score_text)
-		player_two_score_text = createTextMesh(player_two.score.toString(), player_two_score_text, constants.GAME_AREA_WIDTH / 2, 0,-80, 0xf0f0f0, 50);
+		player_two_score_text = createTextMesh(droidFont, player_two.score.toString(), player_two_score_text, constants.GAME_AREA_WIDTH / 2, 0,-80, 0xf0f0f0, 50);
+		scene.add(player_two_score_text)
 	}
 	ball.reset()
 	player_one.reset()
@@ -157,10 +130,15 @@ function winning()
 	scene.remove(player_one_score_text)
 	scene.remove(player_two_score_text)
 	if (player_one.score == constants.WINNING_SCORE)
-		player_one_score_text = createTextMesh("PLAYER 1 WIN", player_one_score_text, 0, 0, 0, 0xffffff, 13)
+	{
+		player_one_score_text = createTextMesh(droidFont, "PLAYER 1 WIN", player_one_score_text, 0, 0, 0, 0xffffff, 13)
+		scene.add(player_one_score_text)
+	}
 	else
-		player_two_score_text = createTextMesh("PLAYER 2 WIN", player_two_score_text, 0, 0, 0, 0xffffff, 13)
-
+	{
+		player_two_score_text = createTextMesh(droidFont, "PLAYER 2 WIN", player_two_score_text, 0, 0, 0, 0xffffff, 13)
+		scene.add(player_two_score_text)
+	}
 }
 
 //GameLoop
